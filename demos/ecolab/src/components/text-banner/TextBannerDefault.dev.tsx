ca44acbe26ceb1643750131a586ea879948395f9
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2026-07-13 13:16:50 -0400

    updated test-banner component

--- demos/ecolab/src/components/text-banner/TextBannerDefault.dev.tsx
+++ demos/ecolab/src/components/text-banner/TextBannerDefault.dev.tsx
@@ -9,9 +9,8 @@ import { getDescriptiveLinkText } from '@/utils/link-text';
 export const Default: React.FC<TextBannerProps> = (props) => {
   const { fields, params, page } = props;
 
-  const { heading, description, link, image } = fields ?? {};
+  const { heading, description, link } = fields ?? {};
   const { excludeTopMargin } = params ?? {};
-  const inline = image?.value?.src ? { '--bg-img': `url(${image?.value.src})` } : {};
   const isPageEditing = page?.mode?.isEditing ?? false;
   if (fields) {
     return (
@@ -19,49 +18,50 @@ export const Default: React.FC<TextBannerProps> = (props) => {
         className={cn(
           'p-5',
           'mt-4',
-          'bg-cover',
-          'bg-center',
-          'bg-img-primary',
-          'text-primary-foreground',
+          'bg-gray-100',
           {
             'mt-0': excludeTopMargin,
             [props?.params?.styles]: props?.params?.styles,
           }
         )}
-        style={inline as React.CSSProperties}
       >
-        <Flex wrap="wrap" align="center" as="div" gap="3" className="px-4 py-6">
+        <Flex
+          direction="col"
+          align="center"
+          justify="center"
+          as="div"
+          gap="3"
+          className="px-4 py-6 text-center"
+        >
           <FlexItem basis="full">
             <h3>
               <Text field={heading} />
             </h3>
           </FlexItem>
-          <FlexItem basis="auto">
-            <p>
+          <FlexItem basis="full">
+            <p className="text-black">
               <Text field={description} />
             </p>
           </FlexItem>
-          <FlexItem basis="auto" grow="1">
+          <FlexItem basis="full">
             {link && (
-              <Flex justify="end">
-                <Button asChild>
-                  <Link
-                    field={
-                      // Enhance link with descriptive text for SEO
-                      !isPageEditing && link?.value?.text
-                        ? {
-                            ...link,
-                            value: {
-                              ...link.value,
-                              text: getDescriptiveLinkText(link, heading?.value),
-                            },
-                          }
-                        : link
-                    }
-                    editable={isPageEditing}
-                  />
-                </Button>
-              </Flex>
+              <Button asChild>
+                <Link
+                  field={
+                    // Enhance link with descriptive text for SEO
+                    !isPageEditing && link?.value?.text
+                      ? {
+                          ...link,
+                          value: {
+                            ...link.value,
+                            text: getDescriptiveLinkText(link, heading?.value),
+                          },
+                        }
+                      : link
+                  }
+                  editable={isPageEditing}
+                />
+              </Button>
             )}
           </FlexItem>
         </Flex>
